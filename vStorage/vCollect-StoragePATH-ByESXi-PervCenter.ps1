5a28bc2b1072779a0bdd22ddc95fb007d86ab322
Saddam ZEMMALI <saddam.zemmali@gmail.com>
2019-01-06 18:23:09 +0100

    Update vCollect-StoragePATH-ByESXi-PervCenter.ps1

--- vStorage/vCollect-StoragePATH-ByESXi-PervCenter.ps1
+++ vStorage/vCollect-StoragePATH-ByESXi-PervCenter.ps1
@@ -17,7 +17,8 @@
 #  vCollect Targeting Variables # 
 ################################# 
 $StartTime = Get-Date
-$dir= "reports\"
+if (-not (Test-Path '.\vCollect-Reports')) { New-Item -Path '.\vCollect-Reports' -ItemType Directory -Force | Out-Null }
+$report= "..\vCollect-Reports"
 $dateF = Get-Date -UFormat "%d-%b-%Y_%H-%M-%S" 
 ##############################
 # Check the required modules #
@@ -51,25 +52,25 @@ check-Module "VMware.PowerCLI"
 #####################################
 #  vCollect Targeting Report Folder # 
 #####################################
-function check-ReportFolder ($dir) {
-    if(!(Test-Path -Path $report$dir )){
-        New-Item -ItemType directory -Path $report$dir
+function vcollect-check-folder ($dir) {
+    if(!(Test-Path -Path $report\$dir )){
+        New-Item -ItemType directory -Path $report\$dir
         Write-Host "New Storage folder created" -f Magenta
-        New-Item -Path $report$dir -Name $dateF -ItemType "directory"
+        New-Item -Path $report\$dir -Name $dateF -ItemType "directory"
         Write-Host "New Work folder created"   -f Magenta
     }
     else{
       Write-Host "Storage Folder already exists" -f Green
-      New-Item -Path $report$dir -Name $dateF -ItemType "directory"
+      New-Item -Path $report\$dir -Name $dateF -ItemType "directory"
       Write-Host "New Work folder created"  -f Magenta
     }    
 }
 
-check-ReportFolder "Hardware"
+vcollect-check-folder  "vCollect-Storage"
 #################################
 #   vSphere Targeting Variables # 
 #################################  
-$vCenterList = Get-Content "vCenter.txt"
+$vCenterList = Get-Content "..\vCenter.txt"
 $username = Read-Host 'Enter The vCenter Username'
 $password = Read-Host 'Enter The vCenter Password' -AsSecureString    
 $vccredential = New-Object System.Management.Automation.PSCredential ($username, $password)
@@ -77,12 +78,12 @@ $vccredential = New-Object System.Management.Automation.PSCredential ($username,
 #################################
 #   vCheck Targeting Variables  # 
 ################################# 
-$ReportInfo="Storage PATH per ESXi for each vCenter Info Report"
+$ReportInfo="Storage PATH for each ESXi per vCenter Info Report"
 
 #################################
 #           LOG INFO            # 
 ################################# 
-$PathH = "reports\Hardware\$dateF"
+$PathH = "$report\vCollect-Storage\$dateF"
 $DCReport = @()
 # XLSX Reports
 $ReportXlsVC = "_fileName_VM_" + (Get-Date -UFormat "%d-%b-%Y-%H-%M") +".xlsx"
@@ -114,63 +115,76 @@ foreach ($vCenter in $vCenterList){
         Write-Host "Gathering ESXi Storage PATH Informations From vCenter: $vCenter"
         $esxihosts = Get-VMHost
         $allhosts=$esxihosts.count
-        Write-Host "There are $allhosts ESXi in vCenter: $vCenter" -Foregroundcolor "Yellow"
+        Write-Host "# There are $allhosts ESXi in vCenter: $vCenter" -Foregroundcolor "Yellow"
 
         ################################
         # Gathering Hosts informations #
         ################################
         Write-Host "Gathering Hosts Informations"   -Foregroundcolor "Cyan"
         $i=0
-        $Report = ForEach ($esxi in $esxihosts) {            
+        $Report = @() 
+        ForEach ($esxi in $esxihosts) {            
             $i++
             Write-Progress -Activity "vCollecting hosts" -Status ("Host: {0}" -f $esxi.Name) -PercentComplete ($i/$allhosts*100) -Id 1  -ParentId 0
             $hbas = $esxi | Get-VMHostHba
             $allhbas=$hbas.count
-            Write-Host "There are $allhbas HBAs in ESXi: $esxi" -Foregroundcolor "Yellow"
+            Write-Host "## There are $allhbas HBAs in ESXi: $esxi" -Foregroundcolor "Yellow"
+            
+            #Datacenter info  
+            $datacenter = $esxi | Get-Datacenter | Select-Object -ExpandProperty name  
+            #Cluster info  
+            $cluster = $esxi | Get-Cluster | Select-Object -ExpandProperty name  
+            #vCenter Server  
+            $vCenter = $esxi.ExtensionData.Client.ServiceUrl.Split('/')[2].trimend(":443") 
+
 
             ###############################
             # Gathering HBAs informations #
             ###############################
-            Write-Host "Gathering HBAs Informations"    -Foregroundcolor "Cyan"
+            Write-Host "Gathering HBAs Informations from ESXi: $esxi"    -Foregroundcolor "Cyan"
             $j=0
             ForEach ($hba in $hbas) {
                 $j++                
                 Write-Progress -Activity "vCollecting HBAs" -Status ("HBA: {0}" -f $hba.Device) -PercentComplete ($j/$allhbas*100) -Id 2  -ParentId 1
                 $scsiluns = $hba | Get-ScsiLun
                 $allluns=$scsiluns.count
-                Write-Host "There are $allluns LUNs in HBA: $hba" -Foregroundcolor "Yellow"
+                Write-Host "### There are $allluns LUNs in HBA: $hba" -Foregroundcolor "Yellow"
 
                 ###############################
                 # Gathering Luns informations #
                 ###############################
-                Write-Host "Gathering Luns Informations"    -Foregroundcolor "Cyan"
+                Write-Host "Gathering Luns Informations from HBA: $hba"    -Foregroundcolor "Cyan"
                 $k=0
                 ForEach ($scsilun in $scsiluns) {
                     $k++
                     Write-Progress -Activity "vCollecting Luns" -Status ("Lun: {0}" -f $scsilun.CanonicalName) -PercentComplete ($k/$allluns*100) -Id 3  -ParentId 2
                     $scsipaths = $scsilun | Get-Scsilunpath
                     $allpaths=$scsipaths.count
-                    Write-Host "There are $allpaths Paths in LUN: $scsilun" -Foregroundcolor "Yellow"
+                    Write-Host "#### There are $allpaths Paths in LUN: $scsilun" -Foregroundcolor "Yellow"
 
                     ################################
                     # Gathering Paths informations #
                     ################################
-                    Write-Host "Gathering Paths Informations"   -Foregroundcolor "Cyan"
+                    Write-Host "Gathering Paths Informations from LUN: $scsilun"   -Foregroundcolor "Cyan"
                     $l=0
                     ForEach ($scsipath in $scsipaths) {
                         $l++                        
                         Write-Progress -Activity "vCollecting Paths" -Status ("Path: {0}" -f $scsipath.Name) -PercentComplete ($l/$allpaths*100) -Id 4  -ParentId 3
-                        New-Object PSObject -Property @{
-                            Host = $esxi.name
-                            HBAName = $scsilun.RuntimeName
-                            PathSelectionPolicy = $scsilun.MultiPathPolicy
-                            Status = $scsipath.state
-                            Source = "{0}" -f ((("{0:x}" -f $hba.PortWorldWideName) -split '([a-f0-9]{2})' | where {$_}) -Join ":")
-                            Target = $scsipath.SanId
-                            LUN = (($scsilun.RunTimeName -Split "L")[1] -as [Int])
-                            Path = $scsipath.LunPath
-                        } 
-                    }           
+
+                        $SPATHInfo = New-Object PSObject  
+                        $SPATHInfo | add-member -MemberType NoteProperty -Name "Data Center"            -Value $datacenter
+                        $SPATHInfo | add-member -MemberType NoteProperty -Name "vCenter"                -Value $vCenter 
+                        $SPATHInfo | add-member -MemberType NoteProperty -Name "Cluster"                -Value $cluster
+                        $SPATHInfo | add-member -MemberType NoteProperty -Name "Host"                   -Value $esxi.name
+                        $SPATHInfo | add-member -MemberType NoteProperty -Name "HBA Name"               -Value $scsilun.RuntimeName
+                        $SPATHInfo | add-member -MemberType NoteProperty -Name "Path Selection Policy"  -Value $scsilun.MultiPathPolicy
+                        $SPATHInfo | add-member -MemberType NoteProperty -Name "Status"                 -Value $scsipath.state
+                        #$SPATHInfo | add-member -MemberType NoteProperty -Name "Source"                 -Value "{0}" -f ((("{0:x}" -f $hba.PortWorldWideName) -split '([a-f0-9]{2})' | where {$_}) -Join ":")
+                        $SPATHInfo | add-member -MemberType NoteProperty -Name "Target"                 -Value $scsipath.SanId
+                        $SPATHInfo | add-member -MemberType NoteProperty -Name "LUN"                    -Value (($scsilun.RunTimeName -Split "L")[1] -as [Int])
+                        $SPATHInfo | add-member -MemberType NoteProperty -Name "Path"                   -Value $scsipath.LunPath 
+                    }
+                $Report += $SPATHInfo                 
                 }
             }
         }
@@ -209,7 +223,6 @@ foreach ($vCenter in $vCenterList){
 	  Write-Host "Error in Disconnecting to $vCenter" -Foregroundcolor "Red" 
 	}
 }
-
 #################################
 #     End vCollect By vCenter   # 
 ################################# 
